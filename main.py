0c34cf4c2bc58c4bc089daa157535f1ec4dedd3a
Omar3yyad25 <52047522+Omar3yyad25@users.noreply.github.com>
2022-12-09 23:42:33 +0200

    probability

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from tkinter import *
+from collections import Counter
+
+
 x= ""
 win=Tk()
 win.geometry("250x100")
 
+def word_freq(string):
+    text = ''.join(e for e in string if e.isalnum())
+    d = Counter(''.join(text))  # count all letters
+    return (dict(d))    # return a tuple of counted words and letters
+
+
 def analysis():
    value=my_text_box.get("1.0","end-1c")
    file = open (value+'.txt', 'r')
+   fileContent=file.read()
+
+   letters =word_freq(fileContent)
+   sumLetters = sum(letters.values())
+   for l in letters:
+        print("Probability of '{}': {}".format(l,letters[l]/sumLetters))
+
 
 my_text_box=Text(win, height=1, width=20)
 my_text_box.pack()
